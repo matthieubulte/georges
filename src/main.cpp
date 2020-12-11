3bd13bb8b3e5efca57df9057840aa34c2982d3f1
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2020-12-12 00:22:45 +0100

    Distance function now depends on the time. This has the following consequences: - We can have animations (yay!) - Managing the render cache of the painters is harder (nooo)
    There is now a trade off between the actual and felt framerate. If the framerate is too high, the painter has only time to update a few pixels at each frame. This means that only a small part of the image will be updated, and since the part is random, it might be a boring time and the image will not update very fast. The frame rate will then feel low.
    
    OTOH with the current frame rate of 10fps (which sounds low), the painters have a lot of time to update a bug chunk of the image between two frames and so it visually feels like the image is updating fast. Of course the image is in absolute terms very slow at updating.

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
 
 #define DEF_RENDER_THREAD(i) std::thread t##i(\
     painter_thread<dimx, dimy, i*pixels_per_thread, (i+1)*pixels_per_thread>,\
-     i, &screen, &camera_pos, &rot, &state.quit, &state_version);
+     i, &screen, &camera_pos, &rot, &state.quit, &frame_number, &time);
 
 using namespace std;
 
@@ -42,18 +42,18 @@ vec3 ray_dir(float fov, const mat3& view_rot, const vec2& size, const vec2& pos 
     return view_rot * dir;
 }
 
-vec2 dist_field(const vec3& p) {
+vec2 dist_field(const float t, const vec3& p) {
     vec3 pt, q;
     float d = max_dist;
     
     // floor
-    pt = p;
+    pt = translate(vec3(0, 0.075*sin(p[0])*sin(p[2]), 0), p);
     d = dist_plane(vec3(0,1,0), 0, pt);
     vec2 rp = vec2(d, /* texture */ 1);
 
     // column
     pt = translate(vec3(0,.75,-1.), p);
-    pt = repeatX(1, pt);
+    pt = repeatX(2, pt);
     d = dist_box(vec3(1, 0.2, 1), pt);
 
     float crad = 0.15*(1 + 0.1*sin(atan2f(pt[0], pt[2])*20.0f));
@@ -76,25 +76,27 @@ vec2 dist_field(const vec3& p) {
     rbb = dist_union(rbb, vec2(d, /* texture */ 3));
 
     // sphere
-    pt = translate(vec3(-1.5,1.5,-0.5), p);
-    // pt = repeatLim(1.1, 1, pt);
-    float freq = 10.0;
-    float rad = 1.0 + 0.1*sin(atan2f(pt[0], pt[1])*10.0f);
+    float st = 3.0f;
+    float y = fmod(t/st, 2.0f)-2*fmod(floorf(t/st), 2.0f)*fmod(t/st,1.0f);
+    y = 1 + (2.0f*fmod(floorf(t/st/2),2.0f)-1)*y;
+
+    pt = translate(vec3(-1.5,.5+y/2,-0.5), p);
+    float rad = .5;
     d = dist_sphere(rad, pt);
     vec2 rs = vec2(d, /* texture */ 2);
     
     return dist_union(rp, rb, dist_union(rbb, rs));
 }
 
-vec3 normal(const vec3& p) {
+vec3 normal(const float t, const vec3& p) {
     float d = 0.5773*0.0001;
     vec3 d1(d,-d,-d);
     vec3 d2(-d,-d,d);
     vec3 d3(-d,d,-d);
     vec3 d4(d,d,d);
 
-    return normalize(d1*dist_field(p+d1)[0] + d2*dist_field(p+d2)[0] + 
-                     d3*dist_field(p+d3)[0] + d4*dist_field(p+d4)[0]);
+    return normalize(d1*dist_field(t, p+d1)[0] + d2*dist_field(t, p+d2)[0] + 
+                     d3*dist_field(t, p+d3)[0] + d4*dist_field(t, p+d4)[0]);
 }
 
 float clamp(float x, float lo, float hi) {
@@ -105,7 +107,7 @@ float ambient(const vec3& p, const vec3& n) {
     return clamp(dot(n, light_dir), 0, 1);
 }
 
-vec3 texture(int texture_id, const vec3& pos) {
+vec3 texture(const float time, int texture_id, const vec3& pos) {
     if (texture_id == 1) { // floor
         float x = pos[0] >= 0 ? pos[0] : -pos[0] + 0.5;
         float z = pos[2] >= 0 ? pos[2] : -pos[2] + 0.5;
@@ -119,12 +121,12 @@ vec3(1,1,1) : vec3(0,0,0);
     return vec3(0,1,0);
 }
 
-vec2 march(const vec3& origin, const vec3& direction, int steps) {
+vec2 march(const float time, const vec3& origin, const vec3& direction, int steps) {
     vec2 res(max_dist, 0);
     float t = 1.0;
 
     for (int s = 0; s < steps && t < max_dist; s++) {
-        res = dist_field(origin + t * direction);
+        res = dist_field(time, origin + t * direction);
         if (res[0] < 0.0005*t) {
             return vec2(t, res[1]);
         }
@@ -134,14 +136,14 @@ vec2 march(const vec3& origin, const vec3& direction, int steps) {
     return vec2(-1, 0);
 }
 
-float shadow(const vec3& p, const vec3& n, int k) {
+float shadow(float time, const vec3& p, const vec3& n, int k) {
     float t = 0.01;
     float h;
     float res = 1.0;
     vec2 dres;
 
     for (int s = 0; s < 64 || t < 6.0; s++) {
-        dres = dist_field(p + t*light_dir);
+        dres = dist_field(time, p + t*light_dir);
         h = dres[0];
         res = min(res, k*max(0.0f, h)/t);
         if (h < 0.0001) {
@@ -166,10 +168,10 @@ vec3 apply_fog(const vec3& original_color, float distance, const vec3& ray_dir, 
     return interp(fog_color, original_color, fog);
 }
 
-color renderPixel(const vec3& camera_pos, const mat3& view_rot, const vec2& dim, const vec2& xy) {
+color renderPixel(const float time, const vec3& camera_pos, const mat3& view_rot, const vec2& dim, const vec2& xy) {
     vec3 dir = ray_dir(45.0, view_rot, dim, vec2(xy));
     vec3 color;
-    vec2 res = march(camera_pos, dir, max_its);
+    vec2 res = march(time, camera_pos, dir, max_its);
 
     float hit_time = res[0];
     float hit_texture = res[1];
@@ -178,9 +180,9 @@ color renderPixel(const vec3& camera_pos, const mat3& view_rot, const vec2& dim,
         color = background_color;
     } else {
         vec3 p = camera_pos + hit_time * dir;
-        vec3 n = normal(p);
+        vec3 n = normal(time, p);
 
-        float sha = clamp(shadow(p+n*0.1, n, 32), 0, 1);
+        float sha = clamp(shadow(time, p+n*0.1, n, 32), 0, 1);
         float sun = clamp(dot(n, light_dir), 0, 1);
         float sky = clamp(0.5 + 0.5*n[1], 0, 1);
         float ind = clamp(dot(n, normalize(light_dir*vec3(-1.0,0.0,-1.0))), 0, 1);
@@ -191,7 +193,7 @@ color renderPixel(const vec3& camera_pos, const mat3& view_rot, const vec2& dim,
         lin = lin + sky * vec3(0.16,0.20,0.28);
         lin = lin + ind * vec3(0.40,0.28,0.20);
 
-        color = lin * texture((int)hit_texture, p);    
+        color = lin * texture(time, (int)hit_texture, p);    
     }
 
     color = apply_fog(color, hit_time, dir, light_dir);
@@ -213,7 +215,6 @@ inline bool is_in_range(unsigned int x, unsigned int y) {
     return min_offset <= offset && offset < max_offset;
 }
 
-
 template<unsigned int dimx, unsigned int dimy, unsigned int min_offset, unsigned int max_offset>
 void splash_color(Screen<dimx, dimy>* screen, const std::vector<bool>& r_buffer, unsigned int x, unsigned int y, const color& c) {
     screen->put_pixel(x, y, std::get<0>(c), std::get<1>(c), std::get<2>(c));
@@ -232,19 +233,19 @@ void splash_color(Screen<dimx, dimy>* screen, const std::vector<bool>& r_buffer,
 }
 
 template<unsigned int dimx, unsigned int dimy, unsigned int min_offset, unsigned int max_offset>
-void painter_thread(unsigned int thread_id, Screen<dimx, dimy>* screen, const vec3* camera_pos, const mat3* camera_rot, bool* quit, int* state_version) {
+void painter_thread(unsigned int thread_id, Screen<dimx, dimy>* screen, const vec3* camera_pos, const mat3* camera_rot, const bool* quit, const unsigned long* frame_number, const float* time) {
     constexpr unsigned int range = max_offset - min_offset;
     const vec2 dim(dimx, dimy);
 
-    int last_state_version = 0;
+    unsigned long current_frame = 0;
 
     std::vector<bool> r_buffer(range);
     std::fill(r_buffer.begin(), r_buffer.end(), false);
 
     while (!*quit) {
-        if (last_state_version != *state_version) {
+        if (current_frame != *frame_number) {
             std::fill(std::begin(r_buffer), std::end(r_buffer), false);
-            last_state_version = *state_version;
+            current_frame = *frame_number;
         }
 
         for (auto i = 0; i < 10000; i++) {
@@ -255,7 +256,7 @@ void painter_thread(unsigned int thread_id, Screen<dimx, dimy>* screen, const ve
             const unsigned int x = offset % dimx;
             const unsigned int y = (offset - x) / dimx;
 
-            color c = renderPixel(*camera_pos, *camera_rot, dim, vec2(x, dimy - y - 1));
+            color c = renderPixel(*time, *camera_pos, *camera_rot, dim, vec2(x, dimy - y - 1));
             r_buffer[local_offset] = true;
 
             splash_color<dimx, dimy, min_offset, max_offset>(screen, r_buffer, x, y, c);
@@ -271,14 +272,14 @@ int main() {
     const float walk_speed = 0.2f;
     const float turn_speed = 0.05f;
     
-    int state_version = 0;
+    unsigned long frame_number = 0;
     vec3 camera_pos = vec3(0.0, 1.0, 4.5);
     float view_rot = 0.0f;
     mat3 rot = rotationY(view_rot);
+    float time = 0;
     
     Screen<dimx, dimy> screen;
     controles_state state;
-    PerformanceMonitor perf_monitor(seconds_between_logs);
     
     if (!screen.initialize("")) {
         return -1;
@@ -296,9 +297,6 @@ int main() {
 
     while(!state.quit) {
         poll_state(state);
-        if (state.down || state.right || state.left || state.up) {
-            state_version++;
-        }
 
         view_rot += (state.left - state.right) * turn_speed;
         view_rot = fmodf(view_rot, 2.0 * M_PI);
@@ -310,8 +308,9 @@ int main() {
         
 
         screen.render();
-        screen.sleep(50);
-        perf_monitor.tick();
+        screen.sleep(100);
+        frame_number++;
+        time += 100.f / 1000.0f;
     }
 
     t0.join(); t1.join(); t2.join(); t3.join();
